b044a793cbf2cfa60c9dc4334299e164cdbdd054
Anton Vasiljev <antono.vasiljev@gmail.com>
2025-07-11 13:15:08 +0300

    more lsp servers

--- config/plugins/lsp/lsp.nix
+++ config/plugins/lsp/lsp.nix
@@ -4,16 +4,61 @@
       enable = true;
       inlayHints = true;
 
-      servers.typos_lsp = {
-        enable = true;
-        extraOptions = {
-          init_options.diagnosticSeverity = "Hint";
+      servers = {
+        typos_lsp = {
+          enable = true;
+          extraOptions = {
+            init_options.diagnosticSeverity = "Hint";
+          };
         };
+        yamlls = {
+          enable = true;
+          settings = {
+            schemaStore = {
+              enable = false;
+              url = "";
+            };
+          };
+        };
+
+        lua_ls = {
+          enable = true;
+          settings.telemetry.enable = false;
+        };
+
+        rust_analyzer = {
+          enable = true;
+          installRustc = true;
+          installCargo = true;
+        };
+
+        ts_ls.enable = true; # TS/JS
+        cssls.enable = true;
+        tailwindcss.enable = true;
+        svelte.enable = true;
+        vuels.enable = true;
+        pyright.enable = true;
+        nil_ls.enable = true; # Nix
+        dockerls.enable = true; # Docker
+        bashls.enable = true;
+
       };
 
       keymaps.lspBuf = {
         "<c-k>" = "signature_help";
         "gi" = "implementation";
+        "gd" = {
+          action = "definition";
+          desc = "Goto Definition";
+        };
+        "gr" = {
+          action = "references";
+          desc = "Goto References";
+        };
+        "<leader>cr" = {
+          action = "rename";
+          desc = "Rename";
+        };
       };
     };
     lint.enable = true;
