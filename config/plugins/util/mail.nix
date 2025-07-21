07d6ccf0c4309361a733ebcc58ed7f97f2637a78
Anton Vasiljev <antono.vasiljev@gmail.com>
2025-07-21 23:39:30 +0300

    mail.nix update

--- config/plugins/util/mail.nix
+++ config/plugins/util/mail.nix
@@ -1,6 +1,23 @@
 { pkgs, ... }:
 {
-  extraPlugins = with pkgs.vimPlugins; [
-    notmuch-vim
+  extraPlugins = [
+    pkgs.vimPlugins.notmuch-vim
+    (pkgs.vimUtils.buildVimPlugin {
+      name = "notmuch";
+      src = pkgs.fetchFromGitHub {
+        owner = "yousefakbar";
+        repo = "notmuch.nvim";
+        rev = "e4b0a6cbbe5e5281f7a6a8fa43c3e776d3eaec64";
+        hash = "sha256-/T8gGRJUCy4mtnl6/ItOLvJPUq39RVj2EGrKoJ+WvOA=";
+      };
+      nvimSkipModules = [
+        "notmuch.cnotmuch"
+      ];
+    })
+  ];
+
+  extraPackages = [
+    pkgs.notmuch
+    pkgs.python3Packages.notmuch
   ];
 }
