c1c6d520548e3b216db45fb7f45cf0da886b9c20
Anton Vasiljev <antono.vasiljev@gmail.com>
2026-05-08 03:10:22 +0300

    ✨ Enhance: Tune smart-splits resize step sizes
    - Increase default resize from 10 to 15 units
    - Increase small resize from 5 to 7 units

--- config/plugins/ui/smart-splits.nix
+++ config/plugins/ui/smart-splits.nix
@@ -9,7 +9,7 @@
         "t"
       ];
       key = "<C-A-l>";
-      action = "<cmd>lua require('smart-splits').resize_right(10)<cr>";
+      action = "<cmd>lua require('smart-splits').resize_right(15)<cr>";
       options.desc = "Increase window width";
     }
 
@@ -19,7 +19,7 @@
         "t"
       ];
       key = "<C-A-h>";
-      action = "<cmd>lua require('smart-splits').resize_left(10)<cr>";
+      action = "<cmd>lua require('smart-splits').resize_left(15)<cr>";
       options.desc = "Decrease window width";
     }
 
@@ -29,7 +29,7 @@
         "t"
       ];
       key = "<C-A-j>";
-      action = "<cmd>lua require('smart-splits').resize_down(10)<cr>";
+      action = "<cmd>lua require('smart-splits').resize_down(15)<cr>";
       options.desc = "Decrease window height";
     }
 
@@ -39,7 +39,7 @@
         "t"
       ];
       key = "<C-A-k>";
-      action = "<cmd>lua require('smart-splits').resize_up(10)<cr>";
+      action = "<cmd>lua require('smart-splits').resize_up(15)<cr>";
       options.desc = "Increase window height";
     }
 
@@ -50,7 +50,7 @@
         "t"
       ];
       key = "<C-A-S-l>";
-      action = "<cmd>lua require('smart-splits').resize_right(5)<cr>";
+      action = "<cmd>lua require('smart-splits').resize_right(7)<cr>";
       options.desc = "Increase window width (small)";
     }
 
@@ -60,7 +60,7 @@
         "t"
       ];
       key = "<C-A-S-h>";
-      action = "<cmd>lua require('smart-splits').resize_left(5)<cr>";
+      action = "<cmd>lua require('smart-splits').resize_left(7)<cr>";
       options.desc = "Decrease window width (small)";
     }
 
@@ -70,7 +70,7 @@
         "t"
       ];
       key = "<C-A-S-j>";
-      action = "<cmd>lua require('smart-splits').resize_down(5)<cr>";
+      action = "<cmd>lua require('smart-splits').resize_down(7)<cr>";
       options.desc = "Decrease window height (small)";
     }
 
@@ -80,7 +80,7 @@
         "t"
       ];
       key = "<C-A-S-k>";
-      action = "<cmd>lua require('smart-splits').resize_up(5)<cr>";
+      action = "<cmd>lua require('smart-splits').resize_up(7)<cr>";
       options.desc = "Increase window height (small)";
     }
   ];
