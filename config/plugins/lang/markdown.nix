338a1d31dda140f134f649bc675ef16a848be461
Anton Vasiljev <antono.vasiljev@gmail.com>
2026-05-25 02:00:45 +0300

    Markdown Review

--- config/plugins/lang/markdown.nix
+++ config/plugins/lang/markdown.nix
@@ -13,6 +13,15 @@
     markdown-preview = {
       enable = true;
       autoLoad = true;
+      # Use fork's `markdown-review` branch (pinned commit) instead of upstream.
+      package = pkgs.vimPlugins.markdown-preview-nvim.overrideAttrs (_: {
+        src = pkgs.fetchFromGitHub {
+          owner = "antono";
+          repo = "markdown-preview.nvim";
+          rev = "683b6c4773c8f58d013d6df7739f709f3a6c22fa"; # branch: markdown-review
+          hash = "sha256-o/cs2+roQMqhr+kdeNYsOWvXMR3DBokQE1Zuejmkh8Y=";
+        };
+      });
     };
 
     # conform-nvim.settings = {
@@ -33,15 +42,24 @@
     };
   };
 
-  # keymaps = [
-  #   {
-  #     mode = "n";
-  #     key = "<leader>m";
-  #     action = "<cmd>MarkdownPreviewToggle<cr>";
-  #     options = {
-  #       silent = true;
-  #       desc = "Toggle markdown preview";
-  #     };
-  #   }
-  # ];
+  keymaps = [
+    {
+      mode = "n";
+      key = "<leader>mp";
+      action = "<cmd>MarkdownPreviewToggle<cr>";
+      options = {
+        silent = true;
+        desc = "Toggle markdown preview";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>mr";
+      action = "<cmd>MarkdownReview<cr>";
+      options = {
+        silent = true;
+        desc = "Toggle markdown Review";
+      };
+    }
+  ];
 }
